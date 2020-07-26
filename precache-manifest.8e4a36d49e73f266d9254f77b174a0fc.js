9ed316ee6af94bfdb18398e01ff7effe67722b98
makhnev-a <ray.makhnev@gmail.com>
2020-07-26 23:33:35 +0300

    Updates

---
+++ precache-manifest.8e4a36d49e73f266d9254f77b174a0fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa17afe4aad97c45cdd7dafc004a759",
+    "url": "/react-test-table/index.html"
+  },
+  {
+    "revision": "9412f3a822dcbc3e1569",
+    "url": "/react-test-table/static/css/main.e2893ff8.chunk.css"
+  },
+  {
+    "revision": "ad11bef6c356cbbe91ac",
+    "url": "/react-test-table/static/js/2.4d979d96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test-table/static/js/2.4d979d96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9412f3a822dcbc3e1569",
+    "url": "/react-test-table/static/js/main.1cf7260f.chunk.js"
+  },
+  {
+    "revision": "b8740abb52ca90cacc82",
+    "url": "/react-test-table/static/js/runtime-main.f2424a4e.js"
+  }
+]);
